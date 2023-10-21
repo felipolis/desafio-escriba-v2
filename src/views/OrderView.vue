f26d9c99b1b73f5ab5fc7e2ea113b1376dc54c8b
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-10-20 21:34:24 -0300

    Adicionando a order view

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,13 +1,24 @@
 <script setup>
-
+import SearchBar from "../components/SearchBar.vue";
 </script>
 
 <template>
-  <div>
-    Order View
-  </div>
+  <main class="order-container">
+    <!-- SEARCH BAR -->
+    <SearchBar />
+    <!-- SEARCH BAR -->
+
+    <!-- TABLE -->
+    <!-- TABLE -->
+  </main>
 </template>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.order-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
 </style>
