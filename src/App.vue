3f1375d229402c6da755e13d2e5e1914ae092443
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-10-20 21:00:33 -0300

    Adicionando o header navigation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script setup>
 import { RouterView } from 'vue-router'
+
+import HeaderNav from './components/HeaderNav.vue'
+import SearchBar from './components/SearchBar.vue'
 </script>
 
 <template>
   <main class="main">
-    <SideBar />
+    <!-- HEADER -->
+    <HeaderNav />
+    <!-- SEARCH BAR -->
+    <SearchBar />
+    <!-- PAGE -->
     <RouterView />
   </main>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.main {
+  background-color: rgb(243 244 246);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  padding: 0.5rem;
+}
 
 </style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,78 @@
+<script setup></script>
+
+<template>
+  <main class="header-container">
+    <!-- LOGO -->
+    <div class="img-container">
+      <img src="../assets/logo.png" alt="" />
+    </div>
+    <!-- LOGO -->
+
+    <!-- NAV -->
+    <div class="nav-links">
+      <router-link to="/pessoas" active-class="active" exact>
+        <i class="fa-regular fa-user"></i>
+        <span>Pessoas</span>
+      </router-link>
+      <router-link to="/produtos" active-class="active" exact>
+        <i class="fa-solid fa-tag"></i>
+        <span>Produtos</span>
+      </router-link>
+      <router-link to="/pedidos" active-class="active" exact>
+        <i class="fa-brands fa-shopify"></i>
+        <span>Pedidos</span>
+      </router-link>
+    </div>
+    <!-- NAV -->
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.header-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 30%;
+  width: 100%;
+  background-color: white;
+  padding: 1rem;
+
+  .img-container {
+    //width: 90%;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .nav-links {
+    width: 100%;
+    list-style: none;
+    padding: 0;
+    margin-top: 2rem;
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: row;
+
+    a {
+      text-decoration: none;
+      color: rgb(63, 63, 63);
+      padding: 1rem;
+      border-radius: 1rem;
+      cursor: pointer;
+      transition: 0.3s;
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+
+      &.active {
+        background-color: #e0e0e0;
+      }
+
+      &:hover {
+        background-color: #f5f5f5;
+      }
+    }
+  }
+}
+</style>
